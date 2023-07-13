581ce216540647310867c6b57b191c86e722cb21
terrencecoding <126296863+terrencecoding@users.noreply.github.com>
2023-07-12 23:37:23 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map((entry) => {
+          return (
+            <Entry
+              key={entry.id}
+              emoji={entry.emoji}
+              name={entry.name}
+              meaning={entry.meaning}
+            />
+          );
+        })}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
